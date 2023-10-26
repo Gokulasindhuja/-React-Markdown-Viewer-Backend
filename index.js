3ec6d33274042220457bd909dd9b7ca6216661b7
Gokulasindhuja <136049113+Gokulasindhuja@users.noreply.github.com>
2023-10-26 22:40:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 import express from "express";
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import playersRouter from "./routes/players.route.js";
 import userRouter from "./routes/user.route.js";
-
-
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 const client = new MongoClient(MONGO_URL);
-await client.connect();
-console.log("MongoDb is connected..!")
 
-app.use(express.json());
-app.use(cors())
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("MongoDb is connected..!");
 
+    app.use(express.json());
+    app.use(cors());
+    app.use('/players', playersRouter);
+    app.use('/users', userRouter);
 
-app.use('/players',playersRouter)
-app.use('/users',userRouter)
+    app.get("/", function (req, res) {
+      res.send("Hello world");
+    });
 
-app.get("/",function(req,res){
-    res.send("Hello world")
-})
+    app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
 
-
-app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
-
-export {client}
+startServer();
